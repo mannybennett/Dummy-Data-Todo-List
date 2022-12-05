5ddd586e93cae5f61545587a55a215fabee32e01
mannybennett <mannybennett99@gmail.com>
2022-12-05 17:28:41 -0500

    update

--- scripts.js
+++ scripts.js
@@ -12,31 +12,62 @@ const list = document.getElementById('todo-list')
 
 const populateTodos = () => {
     for (let i=0; i < arrayOfTodos.length; i++) {
-
-        
         const element = document.createElement('li')
         const text = document.createTextNode(arrayOfTodos[i].title)
         list.appendChild(element)
         element.append(text)
-        
     }
 }
+
+const reset = () => {
+    list.innerHTML = ""
+}
+
+const complete = []
+
+const filteredTodos = () => {
+    
+    reset()
+    
+    let input = document.getElementById("userId").value
+    
+    const filterArray = arrayOfTodos.filter(todos => todos.userId == input)
+
+    console.log(filterArray)
+    
+    for (let i=0; i < filterArray.length; i++) {
+        const element = document.createElement('li')
+        const text = document.createTextNode(filterArray[i].title)
+        list.appendChild(element)
+        element.append(text)
+        complete.push(filterArray)
+
+        console.log(complete)
+    }
+}
+
+const completedTodos = () => {
+    
+    reset()
+    
+    for (let i=0; i < complete.length; i++) {
+        if (complete[i].completed === true) {
+            
+            console.log(complete)
+
+            const element = document.createElement('li')
+            const text = document.createTextNode(complete[i].title)
+            list.appendChild(element)
+            element.append(text)
+            
+        }
+    }
+}
+
+// ***prevents crashing***
 
 const form = document.querySelector("form")
 
 form.addEventListener("submit", e => {
     e.preventDefault();
-    list.innerHTML = "" 
 })
-
-const userInput = () => {
-    let input = document.getElementById("userId").value
-    const completed = true
-
-    fetch('https://jsonplaceholder.typicode.com/todos')
-    .then( (response) => response.json())
-    .then( (todos) => {
-        const arrayOfTodos = todos.filter(todo => todo.userId === input && todo.completed === completed)
-        console.log(arrayOfTodos) })
-}
-
